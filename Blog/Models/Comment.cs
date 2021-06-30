3c485f3afcc6bba5fb921b6dd15defc704ecc81b
0Dexter0 <bsv16082002@gmail.com>
2021-06-30 18:37:50 +0300

    Filling in models

--- Blog/Models/Comment.cs
+++ Blog/Models/Comment.cs
@@ -1,7 +1,23 @@
+using System;
+
 namespace Blog.Models
 {
     public class Comment
     {
+        public long Id { get; init; }
+        public string Content { get; set; }
+        public DateTime Published { get; init; }
+        public DateTime LastEdited { get; set; }
+
+        public Post Post { get; set; }
+        public long PostId { get; init; }
         
+        public User Creator { get; init; }
+        public long CreatorId { get; init; }
+
+        public Comment()
+        {
+            Published = DateTime.Now;
+        }
     }
 }
--- Blog/Models/Post.cs
+++ Blog/Models/Post.cs
@@ -1,7 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 namespace Blog.Models
 {
     public class Post
     {
+        public long Id { get; init; }
+        public string Content { get; set; }
+        public DateTime Published { get; init; }
+        public DateTime LastEdited { get; set; }
+        private List<Comment> Comments { get; set; }
         
+        public User Creator { get; init; }
+        public long CreatorId { get; init; }
+
+        public Post()
+        {
+            Comments = new();
+        }
     }
 }
