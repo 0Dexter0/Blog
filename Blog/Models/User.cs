95d4d1336f7321f4a087e9c8738b81365d88ded0
evastarish <evastarish@gmail.com>
2021-06-30 18:38:15 +0300

    User

--- Blog/Models/User.cs
+++ Blog/Models/User.cs
@@ -2,6 +2,14 @@ namespace Blog.Models
 {
     public class User
     {
-        
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Post> Posts { get; set; }
+
+        public User()
+        {
+        }
     }
 }
